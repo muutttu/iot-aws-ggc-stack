a2588e7af029b7742c895e366cf2d88ae52f88c9
Tuomo Muuttoranta <muutttu@live.fi>
2021-02-11 21:25:49 +0200

    add container id

--- web/app.py
+++ web/app.py
@@ -2,13 +2,16 @@
 
 # Import the flask module
 from flask import Flask
+import socket
 
 # Create a Flask constructor. It takes name of the current module as the argument
 app = Flask(__name__)
 
+docker_short_id = socket.gethostname()
+
 @app.route('/')
 def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+    return 'Hey, we have Flask in a Docker container with id: ' + docker_short_id
 
 
 if __name__ == '__main__':
